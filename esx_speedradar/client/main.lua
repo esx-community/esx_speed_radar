a8fece9ec8ebf02036818b9a127219bba48b9e0c
rex2630 <rex2630@gmail.com>
2020-01-17 22:15:50 +0100

    Minor cleanup

--- esx_speedradar/client/main.lua
+++ esx_speedradar/client/main.lua
@@ -1,22 +1,9 @@
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
-ESX                           = nil
-local GUI                     = {}
-GUI.Time                      = 0
+ESX = nil
+local GUI = {}
+GUI.Time = 0
 local vitesse_flash = 0
 local is_flashed = false
-local PlayerData              = nil
-
+local PlayerData = nil
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -36,15 +23,11 @@ AddEventHandler('esx:setJob', function(job)
 	PlayerData.job = job
 end)
 
-
 function Info(text, loop)
  SetTextComponentFormat("STRING")
  AddTextComponentString(text)
  DisplayHelpTextFromStringLabel(0, loop, 1, 0)
 end
-
-
-
 
 function GetFlashMenu(idstation)
 
@@ -56,10 +39,6 @@ function GetFlashMenu(idstation)
 			rows = {}
 		}
 
-
-
-
-
 		for i=1, #flashlist, 1 do
 
 			table.insert(elements.rows, {
@@ -70,41 +49,22 @@ function GetFlashMenu(idstation)
 					flashlist[i].speed,
 					flashlist[i].modele,
 					' {{[Valider]|recup}} '
-
 				}
 			})
 		end
 
-			ESX.UI.Menu.Open(
-			'list', GetCurrentResourceName(), 'flashlist',
+			ESX.UI.Menu.Open('list', GetCurrentResourceName(), 'flashlist',
 			elements,
 			function(data, menu)
 
 				if data.value == 'recup' then
 					TriggerServerEvent("esx_peage:delete_peage",data.data.id)
 					menu.close()
-
-
 				end
-
-
-
-
-			end,
-			function(data, menu)
+			end, function(data, menu)
 				menu.close()
-			end
-		)
-
-
-
-
-
-
-
+			end)
 	end,idstation)
-
-
 end
 
 Citizen.CreateThread(function()
@@ -119,27 +79,16 @@ Citizen.CreateThread(function()
 			else
 				StopScreenEffect('RaceTurbo')
 			end
-
 	end
 end)
 
-
-
-
 Citizen.CreateThread(function()
 	while true do
-
 		Citizen.Wait(0)
-
-
 
 --SwitchShortMichaelIn
       local PlayerPed = GetPlayerPed(-1)
       local coords_ped = GetEntityCoords(PlayerPed, true)
-
-
-
-
 
   --    if(GetDistanceBetweenCoords(coords_ped,1376.419,669.30, 78.93, true) < 5 and PlayerData.job.name =='police')then
 	if(GetDistanceBetweenCoords(coords_ped,1376.419,669.30, 78.93, true) < 5)then
@@ -150,8 +99,6 @@ Citizen.CreateThread(function()
          end
       end
 
-
-
       if(GetDistanceBetweenCoords(coords_ped, 1368.90, 671.22, 79.912, true) < 50 ) then
 				if(not is_flashed)then
 			        if((GetEntitySpeed(PlayerPed) * 3.6) > 90)then
@@ -160,24 +107,17 @@ Citizen.CreateThread(function()
 			          end
 			          local vehicle_flash = GetVehiclePedIsIn(PlayerPed, true)
 			          local vehicle_flash_plaque = GetVehicleNumberPlateText(vehicle_flash)
-								local vehicule_flash_modele = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle_flash))
+				  local vehicule_flash_modele = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle_flash))
 
 			          is_flashed = true
 			          TriggerServerEvent('esx_peage:flashed',vehicle_flash_plaque,vitesse_flash,vehicule_flash_modele,"1")
 								StartScreenEffect('SwitchShortMichaelIn',  400,  false)
-
 								end
 					end
 			        else
 
 			          vitesse_flash = 0
 			          is_flashed = false
-
-
-
-
       end
-
-
   end
 end)
